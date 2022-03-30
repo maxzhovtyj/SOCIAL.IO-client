f2856ebfb9e1aef578f3bbe1309eb7c1f5713fe8
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2022-03-30 12:12:28 +0300

    useHttp deleted

--- src/components/main/Profile/ProfileContainer.jsx
+++ src/components/main/Profile/ProfileContainer.jsx
@@ -1,9 +1,8 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useCallback, useContext, useEffect, useState} from 'react';
 
 import {useDispatch, useSelector} from "react-redux";
 import {addPostActionCreator} from "../../../redux/profilePageReducer";
 
-import {useHttp} from "../../../hooks/http.hook";
 import {useParams} from "react-router-dom";
 
 import Profile from "./Profile";
@@ -12,7 +11,6 @@ import {AuthContext} from "../../../context/AuthContext";
 
 const ProfileContainer = () => {
     const auth = useContext(AuthContext)
-    const {request, loading} = useHttp()
     const params = useParams()
 
     const dispatch = useDispatch()
@@ -22,42 +20,39 @@ const ProfileContainer = () => {
 
     const [isOwner, setIsOwner] = useState(false)
     const [userInfo, setUserInfo] = useState({})
-    const [newPost, setNewPost] = useState('')
+    const [newPostText, setNewPostText] = useState('')
     const [responseStatus, setResponseStatus] = useState(null)
 
     const onSetNewPost = (event) => {
-        setNewPost(event.target.value)
+        setNewPostText(event.target.value)
     }
 
-    const sendNewPost = () => {
+    const sendNewPost = async () => {
         try {
-            let newPostSend = {
-                id: Date.now(), text: newPost, name: 'Max Zhovtaniuk', likes: 0
+            let newPost = {
+                authorId: auth.userId,
+                authorName: userInfo.username,
+                text: newPostText,
             }
-            if (!newPost.trim()) {
+            if (!newPost.text.trim()) {
                 throw new Error("Empty post")
             }
-            let newPostAction = addPostActionCreator(newPostSend)
+            let newPostAction = addPostActionCreator(newPost)
             dispatch(newPostAction)
         } catch (error) {
             console.error(error)
         } finally {
-            setNewPost('')
+            setNewPostText('')
         }
     }
 
     useEffect(() => {
         if (params.id === auth.userId) {
             setIsOwner(true)
-        }
-        else setIsOwner(false)
+        } else setIsOwner(false)
 
         async function fetchData() {
             try {
-                const response = await request(`/auth/userInfo?id=${params.id}`)
-                const userPostsResponse = await request(`/auth/userPosts?id=${params.id}`)
-                setUserPosts(userPostsResponse)
-                setUserInfo(response)
                 setResponseStatus('ok')
             } catch (e) {
                 setResponseStatus(null)
@@ -68,14 +63,11 @@ const ProfileContainer = () => {
         fetchData()
     }, [params.id, isOwner])
 
-    if (loading) {
-        return <ProfileSkeleton/>
-    }
-
-    else if (responseStatus && !loading) {
-        return <Profile isOwner={isOwner} profileInfo={userInfo} newPost={newPost} isOwner={isOwner} onSetNewPost={onSetNewPost} sendNewPost={sendNewPost} posts={userPosts}/>
-    }
-    else {
+    if (false) return <ProfileSkeleton/>
+    else if (responseStatus) {
+        return <Profile isOwner={isOwner} profileInfo={userInfo} newPost={newPostText} onSetNewPost={onSetNewPost}
+                        sendNewPost={sendNewPost} posts={userPosts}/>
+    } else {
         return "User was not found"
     }
 }
